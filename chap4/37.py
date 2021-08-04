0d64cd243a28ddb89636fb7fe9c7b752c9efadbc
yu-hiro <83101363+yu-hiro@users.noreply.github.com>
2021-08-04 14:37:59 +0900

    Update 37.py

--- chap4/37.py
+++ chap4/37.py
@@ -1,27 +1,9 @@
+from chap4_neko import neko_mapping
 from collections import defaultdict
 import matplotlib.pyplot as plt
 import japanize_matplotlib
 
 filename = 'data/neko.txt.mecab'
-
-def neko_mapping(filename):
-    neko = []
-    morphs = []
-    with open(filename, 'r', encoding='utf-8_sig') as r: # Windows上で実行したので、'utf-8_sig'で指定
-        for line in r: # 1行ずつ読み込む
-            if line != 'EOS\n': # 文末でない場合
-                fields = line.split('\t')
-                if len(fields) != 2 or fields[0] == '': # 文頭以外の空白と改行文字はスキップ
-                    continue
-                else:
-                    attr = fields[1].split(',')
-                    morph = {'surface': fields[0], 'base': attr[6],
-                             'pos': attr[0], 'pos1': attr[1]}
-                    morphs.append(morph)
-            else:
-                neko.append(morphs)
-                morphs = []
-    return neko
 
 # 「猫」と共起頻度が高い単語
 def neko_co_freq(sentences):
