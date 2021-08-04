67594e1898736d046918d0ecffb025958c8c80b1
yu-hiro <83101363+yu-hiro@users.noreply.github.com>
2021-08-04 14:35:17 +0900

    Update 33.py

--- chap4/33.py
+++ chap4/33.py
@@ -1,23 +1,6 @@
-filename = 'data/neko.txt.mecab'
+from chap4_neko import neko_mapping
 
-def neko_mapping(filename):
-    neko = []
-    morphs = []
-    with open(filename, 'r') as r:
-        for line in r: # 1行ずつ読み込む
-            if line != 'EOS\n': # 文末でない場合
-                fields = line.split('\t')
-                if len(fields) != 2 or fields[0] == '': # 文頭以外の空白と改行文字はスキップ
-                    continue
-                else:
-                    attr = fields[1].split(',')
-                    morph = {'surface': fields[0], 'base': attr[6],
-                             'pos': attr[0], 'pos1': attr[1]}
-                    morphs.append(morph)
-            else:
-                neko.append(morphs)
-                morphs = []
-    return neko
+filename = 'data/neko.txt.mecab'
 
 def main():
     sentences = neko_mapping(filename)
